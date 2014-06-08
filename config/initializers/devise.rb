2eb4af3ae39aa02130fbb209e625d7d510273820
Alexander Panasenkov <aapanasenkov@gmail.com>
2014-06-08 23:26:41 +0400

    new vk app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,7 +231,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :vkontakte, '4392350', 'T0wl5fYqeVzW03iwucSJ'
+  config.omniauth :vkontakte, '4404230', 'unlY20UlYrYCMLM6yB3V'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
